d39d5680060e24119d11bd2da3362f03a06f8d61
Andrew <vishnyaishere@gmail.com>
2022-09-05 17:53:37 +0300

    exc010 done

--- exc010_hw/Program.cs
+++ exc010_hw/Program.cs
@@ -5,3 +5,59 @@
 // 11 16 15 06
 // 10 09 08 07
 
+int[,] FillSpiral2D(int verticalLength = 4, int horizontalLength = 4)
+{
+    int[,] array2D = new int[verticalLength, horizontalLength];
+
+    int count = 1;
+    int i = 0;
+    int k = 0;
+    int j;
+
+    while (i < verticalLength * horizontalLength)
+    {
+        k++;
+
+        for (j = k - 1; j < horizontalLength - k + 1; j++)
+        {
+            array2D[k - 1, j] = count++;
+            i++;
+        }
+
+        for (j = k; j < verticalLength - k + 1; j++)
+        {
+            array2D[j, horizontalLength - k] = count++;
+            i++;
+        }
+
+        for (j = horizontalLength - k - 1; j >= k - 1; j--)
+        {
+            array2D[verticalLength - k, j] = count++;
+            i++;
+        }
+
+        for (j = verticalLength - k - 1; j >= k; j--)
+        {
+            array2D[j, k - 1] = count++;
+            i++;
+        }
+    }
+
+    return array2D;
+}
+
+void PrintArray2D(int[,] array2D)
+{
+    for (int i = 0; i < array2D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2D.GetLength(1); j++)
+        {
+            Console.Write(array2D[i, j].ToString("D2") + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] spiral = FillSpiral2D();
+
+PrintArray2D(spiral);
